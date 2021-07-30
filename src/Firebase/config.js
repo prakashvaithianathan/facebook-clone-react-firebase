1b890a83d303b0cb72f58113254225c8e61e748e
prakashvaithianathan <60776039+prakashvaithianathan@users.noreply.github.com>
2021-07-30 22:44:03 +0530

    Delete config.js

--- src/Firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDSrgMqtdd2_N1KsaKaZFsa1QwmVE1dRqI",
-    authDomain: "facebook-sparkz.firebaseapp.com",
-    projectId: "facebook-sparkz",
-    storageBucket: "facebook-sparkz.appspot.com",
-    messagingSenderId: "133037894518",
-    appId: "1:133037894518:web:8519640f74f3f92328e857"
-  };
